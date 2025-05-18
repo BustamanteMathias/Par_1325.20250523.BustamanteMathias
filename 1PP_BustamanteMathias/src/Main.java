4fdb501810cad83d714fa5e1426ce12a90555b11
Bustamante Mathias <bustamantemathias.tec@gmail.com>
2025-05-18 20:55:13 -0300

    Commit inicial para 1PP

--- 1PP_BustamanteMathias/src/Main.java
+++ 1PP_BustamanteMathias/src/Main.java
@@ -2,6 +2,6 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Commit Inicial Para 1PP");
+        System.out.println("Commit Inicial Para 1 PP");
     }
 }
