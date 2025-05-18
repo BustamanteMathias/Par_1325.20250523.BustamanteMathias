ac79a6eac6cd5bee98aee36c7f16e19b4462b618
Bustamante Mathias <bustamantemathias.tec@gmail.com>
2025-05-18 20:52:04 -0300

    Commit inicial para 1PP

---
+++ 1PP_BustamanteMathias/src/Main.java
@@ -0,0 +1,7 @@
+// 1PP - Comision 325 - Bustamante Mathias Andres.
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Commit Inicial Para 1PP");
+    }
+}
