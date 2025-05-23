dedc1ce5683e4189337e55d54c46ae4917f747db
Bustamante Mathias <bustamantemathias.tec@gmail.com>
2025-05-23 16:54:53 -0300

    Menu base parcial

--- 1PP_BustamanteMathias/src/Main.java
+++ 1PP_BustamanteMathias/src/Main.java
@@ -1,8 +1,40 @@
 // 1PP - Comision 325 - Bustamante Mathias Andres.
 
+import Utilidades_UTN.Controlador;
+import Utilidades_UTN.ManejadorDeDatos;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Commit Inicial Para 1 PP");
-        System.out.println("Prueba push");
+        int opcion;
+        final String MAIN_MENU = """
+                [TITULO]
+                 \t[1] Opcion 1.
+                 \t[2] Opcion 2.
+                 \t[3] Opcion 3.
+                 \t[4] Opcion 4.
+                 \t[5] Opcion 5.
+                \t\t[0] SALIR.
+                \s
+                [OPCION]:\s""";
+
+        do {
+            opcion = ManejadorDeDatos.getIntRange(MAIN_MENU, "[ERROR] Opcion no valida.\n", 0, 5);
+            switch (opcion) {
+                case 0:
+                    System.out.println("[PROGRAMA FINALIZADO]");
+                    break;
+                case 1:
+                    Controlador.Controller_Opcion_01();
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+            }
+        } while (opcion != 0);
     }
 }
---
+++ 1PP_BustamanteMathias/src/Utilidades_UTN/Controlador.java
@@ -0,0 +1,8 @@
+package Utilidades_UTN;
+//import Entidades.*;
+
+public class Controlador {
+    public static void Controller_Opcion_01() {
+        System.out.println("Opcion 1");
+    }
+}
